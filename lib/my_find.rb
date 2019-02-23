a92fc0e7f37810055758059d3db2023cff1955ce
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-23 00:49:13 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   i = 0 
   found_items = []
   while i < collection.length 
-  found_items << yield collection[i]
+  found_items << yield(collection[i])
   i += 1 
 end 
 found_items
