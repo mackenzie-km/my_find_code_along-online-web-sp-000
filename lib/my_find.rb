26cefb7c020809f52738c4ddecc3371fd5bc4876
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-23 00:55:05 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-  if yield(collection[i]) == true
+if yield(collection[i]) == true
    return collection[i] 
-   break
   else
     i += 1 
 end 
